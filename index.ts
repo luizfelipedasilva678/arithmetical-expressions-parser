e967fc8107f5a2ab660bab454392a6ba8a859eef
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2024-05-31 11:27:05 -0300

    Adding validation

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import ShuntingYard from "./shunting-yard";
 
 const parser = new ShuntingYard();
 
-const parsedExpression = parser.transform("3 + 4 * 2 / ( 1 - 5 ) ^ 2");
-
-console.log(parser.resolve(parsedExpression));
+try {
+  console.log(parser.resolve("3 + 4 * 2 / ( 1 - 5 ) ^ 2"));
+} catch (e: any) {
+  console.log(e.message);
+}
